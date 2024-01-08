d05205a7ce41682e77bea542c33b560f87ffb9c2
greg-502 <gp.israel@icloud.com>
2024-01-07 20:00:15 -0600

    Funcionando con Postgresql

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $players = Player::all();
+        return response()->json($players);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+        ]);
+
+        $player = Player::create($validatedData);
+        return response()->json($player, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Player $player)
+    {
+        return response()->json($player);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Player $player)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+        ]);
+
+        $player->update($validatedData);
+
+        return response()->json($player, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Player $player)
+    {
+        $player->delete();
+        return response()->json(null, 204);
+    }
+}
